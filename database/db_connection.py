66bb973ca521060c209fc7cb4a41487659f4e817
shivaprasa1 <142129928+shivaprasa1@users.noreply.github.com>
2026-04-05 14:33:07 +0530

    Update DB connection to use environment variables for Render

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
 
 def get_sql_connection():
     try:
         connection = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="root", # User must update this!
-            database="grocery_store"
+            host=os.environ.get('DB_HOST', 'localhost'),
+            user=os.environ.get('DB_USER', 'root'),
+            password=os.environ.get('DB_PASSWORD', 'root'),
+            database=os.environ.get('DB_NAME', 'grocery_store')
         )
         return connection
     except mysql.connector.Error as err:
